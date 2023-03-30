d3e3033e77c5bfd8f1852a1cdfea1cadead76f9d
HIMJU <hinge1128@gmail.com>
2023-03-30 11:51:08 +0900

    230330

--- src/main/java/com/acorn/springboardstudy/mapper/UserMapper.java
+++ src/main/java/com/acorn/springboardstudy/mapper/UserMapper.java
@@ -5,7 +5,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    //로그인, 개인정보조회, 수정, 삭제, 등록
+    //로그인, 개인정보조회, 수정, 삭제, 회원가입, 아이디찾기(email,phone,이름 입력시 아이디),
+    // 비밀번호 찾기(변경=>[email or 핸드폰 인증] 로 변경페이지 반환 (pwUpdate.do)=> 변경Action)
     UserDto findByUId(String uId);
-
+    UserDto findByUIdAndPw(UserDto user);//로그인
+    String findUIdByEmailAndPhoneAndName(UserDto user);//아이디 찾기
+    int insertOne(UserDto user);
+    int updateOne(UserDto user);
+    int updatePwByUId(UserDto user);//비밀번호변경
+    int deleteByUIdAndPw(UserDto user);//아이디와 비밀번호 정보를 받음. 두개이상의 정보를 받을때는 객체오 받는것도 갠춘
 }
--- src/test/java/com/acorn/springboardstudy/mapper/UserMapperTest.java
+++ src/test/java/com/acorn/springboardstudy/mapper/UserMapperTest.java
@@ -1,16 +1,76 @@
 package com.acorn.springboardstudy.mapper;
 
 import com.acorn.springboardstudy.dto.UserDto;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//MethodOrderer.OrderAnnotation : @Order(0~) 로 순서를 정하겠다.(함수이름 등으로 순서를 정할수 있다.)
 class UserMapperTest {
+//    @BeforeAll//모든 테스트 실행전 실행됨.(초기화)//static 필드
+//    static void init(){}
+//    @AfterAll//모든 테스트가 끝나고 실행 (정리)
+//    static void destory(){}
+//    //@Test : 정의된 함수들은 순서 없이 실행
+//    //등록-> 수정-> 삭제 순 테스트를 기대하나 실제로는 순서를 알 수 없다.
+//    //따라서 초기화 하거나 정리를 해야한다.
+
+    //순서가 있는 TEST -> @TestMethodOrder : test의 순서를 지정할 수 있음 (Junit 5)
     @Autowired
     private UserMapper userMapper;
+    private static UserDto user;
     @Test
+    @Order(1)
+    void insertOne() {
+        user = new UserDto();
+        user.setUId("test00");
+        user.setPw("1234");
+        user.setBirth("1990-11-28");
+        user.setPhone("test-1111-test");
+        user.setEmail("Test00@test.or.com.net");
+        user.setName("테스트");
+        user.setAddress("서울시");
+        user.setDetailAddress("압구정로");
+        user.setGender("NONE");
+        user.setImgPath("/public/imgs/user/test00.jpeg");
+        int insertOne= userMapper.insertOne(user);
+        assertEquals(insertOne,1);
+    }
+    @Test
+    @Order(2)
     void findByUId() {
-        UserDto user=userMapper.findByUId("user01");
-        System.out.println("user = " + user);
+        UserDto finduser=userMapper.findByUId(user.getUId());
+        assertNotNull(finduser);
+    }
+    @Test
+    @Order(3)
+    void findByUIdAndPw() {
+    }
+
+    @Test
+    @Order(4)
+    void findUIdByEmailAndPhoneAndName() {
+    }
+
+    @Test
+    @Order(5)
+    void updateOne() {
+
+    }
+
+
+    @Test
+    @Order(6)
+    void updatePwByUId() {
+    }
+    @Order(7)// 빨리 테스트하기 위해서는 동시에 진행하는것이 가장 좋다 (방법이 없으면 순서를 정한다)
+    @Test
+    void deleteByUIdAndPw() {
+        int deleteByUIdAndPw= userMapper.deleteByUIdAndPw(user);
+        assertEquals(deleteByUIdAndPw,1);
     }
 }
