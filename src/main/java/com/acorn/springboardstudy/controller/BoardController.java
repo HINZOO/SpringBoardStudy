ef5c75ed34c04a608d9287471ff66cbd38c24101
HIMJU <hinge1128@gmail.com>
2023-04-06 09:16:07 +0900

    230405

--- src/main/java/com/acorn/springboardstudy/controller/BoardController.java
+++ src/main/java/com/acorn/springboardstudy/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +23,13 @@ public class BoardController {
         model.addAttribute("boards",boards);
         return "/board/list";
     }
-
+    //?bId=1 //bId 동적페이지에 꼭 필요(400)을 명시적으로 나타내는것
+    @GetMapping("/{bId}/detail.do")
+    public String detail(Model model,
+                         @PathVariable int bId){
+        BoardDto board=boardService.detail(bId);
+        model.addAttribute("b",board);
+        return "/board/detail";//html
+    }
 
 }
