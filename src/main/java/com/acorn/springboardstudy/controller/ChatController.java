dbe9c1f9353fa3e0939d7bd9a9c62d895e2b18b8
HIMJU <hinge1128@gmail.com>
2023-04-26 11:59:37 +0900

    230426

--- src/main/java/com/acorn/springboardstudy/controller/ChatController.java
+++ src/main/java/com/acorn/springboardstudy/controller/ChatController.java
@@ -1,14 +1,14 @@
 package com.acorn.springboardstudy.controller;
 
+import com.acorn.springboardstudy.dto.ChatMsgDto;
 import com.acorn.springboardstudy.dto.ChatRoomDto;
+import com.acorn.springboardstudy.dto.UserDto;
 import com.acorn.springboardstudy.service.ChatMsgService;
 import com.acorn.springboardstudy.service.ChatRoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +38,30 @@ public class ChatController {
             Model model){
         model.addAttribute("crId",crId);
         return "/chat/roomDetail";
+    }
+
+    @GetMapping("/msg/{crId}/list.do") //채팅방상세 메세지들을 json 으로 보낸다.(json 으로 반환할때 @ResponseBody 사용)
+    public @ResponseBody List<ChatMsgDto> list (
+            @PathVariable int crId,
+            @RequestParam(required = false) String postTime
+    ){
+        List<ChatMsgDto> list=null;
+        if(postTime==null){
+             list=chatMsgService.list(crId);
+         }else{
+            list=chatMsgService.list(crId,postTime);
+        }
+
+        return list;//데이터 내역이 json 으로 반환됨(@ResponseBody)
+    }
+    @PostMapping("/msg/register.do")
+    public @ResponseBody int register(
+            @ModelAttribute ChatMsgDto chatMsgDto,
+            @SessionAttribute UserDto loginUser
+    ){//맵핑할 객체지시( @ModelAttribute ChatMsgDto)
+        int register=0;
+        register=chatMsgService.register(chatMsgDto);
+        return register;
     }
 
 }
